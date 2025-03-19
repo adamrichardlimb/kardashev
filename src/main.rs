f36bf1cd24892af0745bf223e0cb6d446190bb13
adamrichardlimb <adamrichardlimb@aol.com>
2025-03-19 23:20:57 +0000

    Moved around some things, canvas has been removed - now we use OpenGL. Input libraries to be set up.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-mod sdl2_utils;
 mod input;
+mod rendering;
 
 use gl;
-use sdl2_utils::Sdl2Utils;
 
 pub fn main() -> Result<(), String> {
 
@@ -10,42 +9,29 @@ pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem
+    let mut window = video_subsystem
         .window("rust-sdl2 example", 800, 600)
         .opengl()
         .build()
         .map_err(|e| e.to_string())?;
 
     //Get the OpenGL context
-    let gl_context = window.gl_create_context().unwrap();
+    let _gl_context = window.gl_create_context().unwrap();
 
     //Point our OpenGL calls to SDL2 so they can be fed to the driver
     let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     //This is our input
-    let event_pump = sdl_context.event_pump()?;
-    let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-    
-    //TODO - Move all this shit to separate rendering and input crates
-    //We should expose standard methods for rendering and input handling so we can write
-    //controllers and people can make fun shaders
-    let mut utils = Sdl2Utils{
-        canvas,
-        event_pump,
-        gl_context
-    };
-
-    
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     'main: loop {
-        
-        let events = utils.poll_events();
+        let events = rendering::poll_events(&mut event_pump);
         
         if events.is_err() {
             break 'main;
         }
 
-        utils.render();
+        rendering::render(&mut window);
     }
 
     Ok(())
--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,12 +1,137 @@
 extern crate gl;
-use gl::types::*;
 
-fn compile_shader(shader_code: &str, shader_type: GLenum) {
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::Canvas, video::{Window, GLContext }, EventPump};
 
+pub struct Sdl2Utils {
+    pub canvas: Canvas<Window>,
+    pub event_pump: EventPump,
+    pub gl_context: GLContext
 }
 
-pub fn load_shader(shader_code: &str, shader_type: GLenum) {
+type Vertex = [f32; 3];
+const VERTICES: [Vertex; 3] = [[-0.5, -0.5, 0.0],[0.5, -0.5, 0.0],[0.0, 0.5, 0.0]];
 
-}
+const VERTEX_SHADER: &str = r#"#version 330 core
+  layout (location = 0) in vec3 pos;
 
+  void main() {
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+  }
+"#;
 
+const FRAGMENT_SHADER: &str = r#"#version 330 core
+  out vec4 final_color;
+
+  void main() {
+    final_color = vec4(0.2, 0.5, 0.2, 1.0);
+  }
+"#;
+
+pub fn render(window: &mut Window) {
+        unsafe {
+
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::ClearColor(0.5, 0.5, 1.0, 1.0);
+
+            //Create the VAO and ensure it is assigned
+            let mut vao = 0;
+            gl::GenVertexArrays(1, &mut vao);
+            assert_ne!(vao, 0);
+
+            //All good? Bind it.
+            gl::BindVertexArray(vao);
+
+            //Create the VBO and ensure it is assigned
+            let mut vbo = 0;
+            gl::GenBuffers(1, &mut vbo);
+            assert_ne!(vbo, 0);
+
+            //All good? Bind it and assign some triangle data.
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                size_of_val(&VERTICES) as isize,
+                VERTICES.as_ptr().cast(),
+                gl::STATIC_DRAW
+            );
+
+            //Tell OpenGL what we will be binding
+            gl::VertexAttribPointer(
+                0,
+                3,
+                gl::FLOAT,
+                gl::FALSE,
+                size_of::<Vertex>().try_into().unwrap(),
+                0 as *const _
+            );
+            gl::EnableVertexAttribArray(0);
+
+            //Now for our shaders
+            let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+            assert_ne!(vertex_shader, 0);
+            gl::ShaderSource(
+                vertex_shader,
+                1,
+                &(VERTEX_SHADER.as_bytes().as_ptr().cast()),
+                &(VERTEX_SHADER.len().try_into().unwrap())
+            );
+            gl::CompileShader(vertex_shader);
+            let mut success = 0;
+            gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+
+            assert_ne!(success, 0);
+
+            //And the fragment shader
+            let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+            assert_ne!(fragment_shader, 0);
+            gl::ShaderSource(
+                fragment_shader,
+                1,
+                &(FRAGMENT_SHADER.as_bytes().as_ptr().cast()),
+                &(FRAGMENT_SHADER.len().try_into().unwrap())
+            );
+            gl::CompileShader(fragment_shader);
+            let mut success = 0;
+            gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success);
+
+            assert_ne!(success, 0);
+
+            //Finally link up the shader program
+            let shader_program = gl::CreateProgram();
+            assert_ne!(shader_program, 0);
+            gl::AttachShader(shader_program, vertex_shader);
+            gl::AttachShader(shader_program, fragment_shader);
+            gl::LinkProgram(shader_program);
+
+            let mut success = 0;
+            gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
+
+            assert_ne!(success, 0);
+
+            gl::DeleteShader(vertex_shader);
+            gl::DeleteShader(fragment_shader);
+
+            gl::UseProgram(shader_program);
+
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+
+        // Show it on the screen
+        window.gl_swap_window();
+    }
+
+    pub fn poll_events(event_pump: &mut EventPump) -> Result<(), String> {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => return Err("User wishes to close program.".to_string()),
+                _ => return Ok(())
+            }
+        }
+
+        return Ok(());
+    }
