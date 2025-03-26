5630550feeae255f1f6db511d0c548b8b83812c7
adamrichardlimb <adamrichardlimb@aol.com>
2025-03-26 13:48:55 +0000

    Set voxel sizes in my chunks and added a simple immutable Chunk iterator.

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -1,16 +1,17 @@
 use crate::world::Mesh;
 
+pub const VOXEL_SIZE: f32 = 0.1;
 pub const CHUNK_SIZE: usize = 16;
 
 const CUBE_VERTICES: &[f32] = &[
-    -0.5, -0.5, -0.5, //Back, bottom left
-     0.5, -0.5, -0.5, //Back, bottom right
-     0.5,  0.5, -0.5, //Back, top right
-    -0.5,  0.5, -0.5, //Back, top left
-    -0.5, -0.5,  0.5, //Front, bottom left
-     0.5, -0.5,  0.5, //Front, bottom right
-     0.5,  0.5,  0.5, //Front, top right
-    -0.5,  0.5,  0.5  //Front, top left
+    -1.0, -1.0, -1.0,
+     1.0, -1.0, -1.0,
+     1.0,  1.0, -1.0,
+    -1.0,  1.0, -1.0,
+    -1.0, -1.0,  1.0,
+     1.0, -1.0,  1.0,
+     1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0
 ];
 
 const CUBE_INDICES: &[u32] = &[
@@ -47,22 +48,18 @@ impl Chunk {
         let mut vertices = Vec::new();
         let mut indices = Vec::new();
 
-        for x in 0..CHUNK_SIZE {
-            for y in 0..CHUNK_SIZE {
-                for z in 0..CHUNK_SIZE {
-                    //If air then just continue
-                    if self.blocks[x][y][z] == 0 {
-                        continue;
-                    }
-
-                    //Otherwise lets make a mesh for the chunk
-                    //Right now this is just a terrible implementation as Proof of Concept
-                    let base_index = (vertices.len() / 3) as u32;
-                    let cube = unit_cube_vertices((x, y, z));
-                    vertices.extend_from_slice(&cube);
-                    indices.extend_from_slice(&unit_cube_indices(base_index));
-                }
+        for ((x,y,z), block) in self.into_iter() {
+           //If air then just continue
+            if block == 0 {
+                continue;
             }
+
+            //Otherwise lets make a mesh for the chunk
+            //Right now this is just a terrible implementation as Proof of Concept
+            let base_index = (vertices.len() / 3) as u32;
+            let cube = unit_cube_vertices((x, y, z));
+            vertices.extend_from_slice(&cube);
+            indices.extend_from_slice(&unit_cube_indices(base_index));
         }
 
         Mesh::from_vertices_and_indices(&vertices, &indices)
@@ -75,9 +72,9 @@ fn unit_cube_vertices(offset: (usize, usize, usize)) -> [f32; 3*8] {
     //Every slice is a vertex in the VBO
     for (i, vertex) in CUBE_VERTICES.chunks(3).enumerate() {
         let base = i* 3;
-        vertices[base + 0] = vertex[0] + offset.0 as f32;
-        vertices[base + 1] = vertex[1] + offset.1 as f32;
-        vertices[base + 2] = vertex[2] + offset.2 as f32;
+        vertices[base + 0] = (vertex[0] * VOXEL_SIZE/2.0) + (offset.0 as f32 * VOXEL_SIZE);
+        vertices[base + 1] = (vertex[1] * VOXEL_SIZE/2.0) + (offset.1 as f32 * VOXEL_SIZE);
+        vertices[base + 2] = (vertex[2] * VOXEL_SIZE/2.0) + (offset.2 as f32 * VOXEL_SIZE);
     }
 
     return vertices;
@@ -91,4 +88,45 @@ fn unit_cube_indices(base_index: u32) -> [u32; 36] {
     }
 
     return indices;
+}
+
+impl<'a> IntoIterator for &'a Chunk {
+    type Item = ((usize, usize, usize), u8);
+    type IntoIter = ChunkIter<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ChunkIter {chunk: self, x: 0, y: 0, z: 0}
+    }
+}
+
+pub struct ChunkIter<'a> {
+    chunk: &'a Chunk,
+    x: usize,
+    y: usize,
+    z: usize
+}
+
+impl<'a> Iterator for ChunkIter<'a> {
+    type Item = ((usize, usize, usize), u8);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.y >= CHUNK_SIZE {
+            return None;
+        }
+
+        let pos = (self.x, self.y, self.z);
+        let value = self.chunk.blocks[self.x][self.y][self.z];
+
+        self.x += 1;
+        if self.x >= CHUNK_SIZE {
+            self.x = 0;
+            self.z += 1;
+            if self.z >= CHUNK_SIZE {
+                self.z = 0;
+                self.y += 1;
+            }
+        }
+
+        Some((pos, value))
+    }
 }
--- src/world/mod.rs
+++ src/world/mod.rs
@@ -3,26 +3,6 @@ mod chunk;
 use crate::rendering::mesh::Mesh;
 use crate::world::chunk::Chunk;
 
-const CUBE_VERTICES: &[f32] = &[
-    -0.5, -0.5, -0.5, //Back, bottom left
-     0.5, -0.5, -0.5, //Back, bottom right
-     0.5,  0.5, -0.5, //Back, top right
-    -0.5,  0.5, -0.5, //Back, top left
-    -0.5, -0.5,  0.5, //Front, bottom left
-     0.5, -0.5,  0.5, //Front, bottom right
-     0.5,  0.5,  0.5, //Front, top right
-    -0.5,  0.5,  0.5  //Front, top left
-];
-
-const CUBE_INDICES: &[u32] = &[
-    0,1,2,2,3,0, //Back
-    4,5,6,6,7,4, //Front
-    4,0,3,3,7,4, //Left
-    1,5,6,6,2,1, //Right
-    3,2,6,6,7,3, //Top
-    0,1,5,5,4,0, //Bottom
-];
-
 pub struct World {
     pub meshes: Vec<Mesh>,
 }
